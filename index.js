213a20e9bc18b2ccdd99e8b1a38968b13fbbfae3
VINISH06 <svinish006@gmail.com>
2023-10-16 19:56:56 +0530

    uuuupdate

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const btn = document.getElementById("Cv").addEventListener("click", () => {
   let link = document.createElement("a");
-  link.href = "assets/ResumeVinish.pdf";
+  link.href = "assets/ResumeVinish_15_10_2023.pdf";
   link.download = "ResumeVinish.pdf";
   link.click();
 });
