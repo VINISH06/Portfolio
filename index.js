32902ca6f239f7705e43a782b8da0af369057162
VINISH06 <svinish006@gmail.com>
2023-10-16 19:56:56 +0530

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const btn = document.getElementById("Cv").addEventListener("click", () => {
   let link = document.createElement("a");
-  link.href = "assets/ResumeVinish.pdf";
+  link.href = "assets/Resume_Vinish (2).docx";
   link.download = "ResumeVinish.pdf";
   link.click();
 });
