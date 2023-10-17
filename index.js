f94ee495dc8af7320cdc74b57d875104c422cc3f
VINISH06 <svinish006@gmail.com>
2023-10-17 11:29:50 +0530

    time footer

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ const btn = document.getElementById("Cv").addEventListener("click", () => {
   link.download = "ResumeVinish.pdf";
   link.click();
 });
+function updateTime() {
+  const date = new Date();
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  const seconds = date.getSeconds().toString().padStart(2, "0");
+  document.getElementById(
+    "Time"
+  ).textContent = `${hours}:${minutes}:${seconds}`;
+}
+setInterval(updateTime, 1000);
+updateTime();
+
+
